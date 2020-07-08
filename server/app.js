b2d1b8cc1168e9a2d8b8a911d384a0ed3499c9fa
Nunzio Ruffo <nunzio.ruffo@sigma3ds.com>
2020-07-08 01:46:04 -0500

    add comments to app.js

--- server/app.js
+++ server/app.js
@@ -1,20 +1,30 @@
 var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+var express     = require('express');
+var path        = require('path');
+var logger      = require('morgan');
+
+
+/**
+* 
+* DB Connection
+*
+**/
 
 const db = require('./config/database');
 db.connect();
 
-/*
+
+/**
+* 
 * Routes
+*
 **/
+
 const users = require('./routes/users');
 const auth = require('./routes/auth');
 
 
 var app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,19 +35,34 @@ app.get('/', function (req, res) {
 });
 
 
-/*
-* URLs
+/**
+* 
+* URls
+*
 **/
+
 app.use('/', auth);
 app.use('/users', users)
 
 
-// catch 404 and forward to error handler
+
+/**
+* 
+* Catch 404 and forward to error handler
+*
+**/
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
+/**
+* 
+* Error Handler
+*
+**/
+
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
